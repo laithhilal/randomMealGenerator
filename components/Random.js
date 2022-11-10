57c5a8fa6858fd27b4a7753c329218bb8b7285ef
laith-21 <112098590+laith-21@users.noreply.github.com>
2022-11-10 18:09:13 +0100

    added insgredients

--- components/Random.js
+++ components/Random.js
@@ -10,6 +10,26 @@ const Random = () => {
     fetchRandom();
   }, [fetchRandom])
 
+
+  const ingredients = [];
+  randomMeal.map(meal => { 
+  for (const property in meal) {
+      if (meal[property] === null || meal[property] === "" || meal[property] === " ")
+        delete meal[property];
+  }
+  for (const property in meal) {
+    if (property.includes("Ingredient")) {
+      const matches = property.match(/(\d+)/);
+      if (!matches) continue;
+      const ingredientNum = matches[0];
+      ingredients.push({
+        ingredient: meal[`strIngredient${ingredientNum}`],
+        measure: meal[`strMeasure${ingredientNum}`],
+      });
+    }
+  }
+})
+
   return (
     <div className='random'>
       {randomMeal.map(meal => (
@@ -18,10 +38,19 @@ const Random = () => {
             <img src={meal.strMealThumb} alt='#' />
             <button onClick={fetchRandom}>Generate another meal</button>
           </div>
+          <div className='random-grid-ingredients'>
+          <h3>Ingredients</h3>
+            <ul>
+            {ingredients? (
+              ingredients.map(ing => <li>{ing.ingredient} - {ing.measure}</li>)
+            ) : (<h4>Ingredients not available</h4>)}
+            </ul>
+          </div>
           <div className='random-grid-instructions'>
             <h3>Instructions</h3>
             <p>{meal.strInstructions}</p>
           </div>
+
         </div>
       ))}
     </div>
